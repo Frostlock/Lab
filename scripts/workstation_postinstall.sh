aa691690350c6da9c4ca8a1ca4fd86619deecc3d
Frostlock <pieter.boogaerts@gmail.com>
2018-04-05 21:25:40 +0200

    Added -y and -q flags

--- scripts/workstation_postinstall.sh
+++ scripts/workstation_postinstall.sh
@@ -3,7 +3,7 @@
 ####################################
 # Update the system
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y -q
 # Set timezone
 # timedatectl list-timezones
 sudo timedatectl set-timezone Europe/Brussels
@@ -12,28 +12,28 @@ sudo timedatectl set-timezone Europe/Brussels
 # REMOTE DESKTOP #
 ##################
 # Install a desktop environment and launch it via .xsession
-#sudo apt-get install xfce4
+#sudo apt-get install xfce4 -y -q
 #echo xfce4-session >~/.xsession
 # Optional: install some extra software packages
-#sudo apt-get install xfce4-terminal, mousepad
+#sudo apt-get install xfce4-terminal, mousepad -y -q
 
 # Install a desktop environment and launch it via .xsession
-sudo apt-get install lxde
+sudo apt-get install lxde -y -q
 echo "exec startlxde" >~/.xsession
 
 # Install the rdp server
-sudo apt-get install xrdp
+sudo apt-get install xrdp -y -q
 
 ##################
 # EXTRA SOFTWARE #
 ##################
 # Pycharm
-#sudo apt-get install default-jre
+#sudo apt-get install default-jre -y -q
 #sudo snap install pycharm-community --classic
 # Firefox browser
-#sudo apt-get install firefox
+#sudo apt-get install firefox -y -q
 # Chrome browser (https://www.ubuntuupdates.org/ppa/google_chrome)
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-stable -y -q
