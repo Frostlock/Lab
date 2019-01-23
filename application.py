15621eb11682f65c9109412e63a66317426da881
Frostlock <pieter.boogaerts@gmail.com>
2019-01-23 11:35:01 +0100

    Subfolder test

--- application.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Frostlocks Github!\n V2.0"
--- webapp-test/application.py
+++ webapp-test/application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Frostlocks Github!<br> This is a subfolder :) <br> V2.3"
+    return "Hello from Frostlocks Github!<br> This is a subfolder :) <br> V2.4"
