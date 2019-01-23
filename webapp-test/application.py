757c23dd087852b02fc9dacfc35b82a3c4e15ce0
Frostlock <pieter.boogaerts@gmail.com>
2019-01-23 20:06:54 +0100

    Update application.py

--- webapp-test/application.py
+++ webapp-test/application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Frostlocks Github!<br> This is a subfolder :) <br> V2.6 - reduced dependencies"
+    return "Hello from Frostlocks Github!<br>This is a simple Flask web app from a subfolder in my Github repository.<br>V3.0"
