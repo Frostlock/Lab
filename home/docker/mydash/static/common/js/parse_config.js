3991a2a233673f8c78d2e7ff6c3cd6fb38514b53
Frostlock <pieter.boogaerts@gmail.com>
2019-07-20 11:41:59 +0200

    mydash v1

---
+++ home/docker/mydash/static/common/js/parse_config.js
@@ -0,0 +1,30 @@
+var request = new XMLHttpRequest();
+request.overrideMimeType("application/json");
+request.open("GET", 'config.json');
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        var config = JSON.parse(this.response);
+        console.log(config);
+
+        // Set page title
+        document.title = config.title;
+
+        // Hide github corner
+        if (config.hide_corner == true) {
+            document.getElementById("corner").style.visibility = 'hidden'
+        }
+
+        // Generate icons
+        var itemlistHTML = '';
+        for (var i = 0; i < config.items.length; i++) {
+            var item = config.items[i];
+            itemlistHTML += '<a href="'+item.link+'" title="'+item.alt+'"><i class="'+item.icon+' fa-fw"></i></a>';
+        }
+        document.getElementById("itemlist").innerHTML = itemlistHTML;
+    } else {
+        var error_text = "Error: "+request.status;
+        console.error(error_text);
+        document.title = error_text;
+    }
+}
+request.send(null);
