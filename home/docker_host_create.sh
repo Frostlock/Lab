78ad0a7bd02ec28111165a2517caa1ea74052ec6
Frostlock <pieter.boogaerts@gmail.com>
2019-07-26 15:47:53 +0200

    host creation script

--- home/docker_host_create.sh
+++ home/docker_host_create.sh
@@ -12,6 +12,7 @@
 LXD_PROFILE=bridgeprofile
 LXD_IMAGE=images:ubuntu/xenial/amd64
 HOST=docker
+DUPLICACY=duplicacy_linux_x64_2.2.3
 
 # Old code to have a fixed IP and a variant hostname.
 #HOST=build-`date +%Y%m%d-%H%M%S`;
@@ -63,3 +64,36 @@ sleep 5;
 #lxc start $HOST;
 ## Sleep for a while to ensure all subsystems are up properly
 #sleep 5;
+
+# Patch the new host
+echo -e "\e[32m---> Upgrading new docker host: $HOST \e[39m";
+lxc exec $HOST -- sh -c " \
+  apt-get -qq update; \
+  apt-get -qq -y upgrade; \
+  apt-get -qq -y install wget nano; \
+";
+
+# Install docker-ce from repositories
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+echo -e "\e[32m---> Installing docker pre-reqs. \e[39m";
+lxc exec $HOST -- sh -c "apt-get -qq -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common";
+echo -e "\e[32m---> Adding docker repo key \e[39m";
+lxc exec $HOST -- sh -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -";
+echo -e "\e[32m---> Adding docker repositories \e[39m";
+lxc exec $HOST -- sh -c "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"";
+echo -e "\e[32m---> Installing docker \e[39m";
+lxc exec $HOST -- sh -c "apt-get -qq update";
+lxc exec $HOST -- sh -c "apt-get -qq -y install docker-ce docker-ce-cli containerd.io";
+echo -e "\e[32m---> Wait for docker daemon to activate properly... \e[39m";
+sleep 5;
+echo -e "\e[32m---> Testing docker \e[39m";
+lxc exec $HOST -- sh -c "docker run hello-world";
+
+# Download duplicacy binary, don't rename it :)
+# https://forum.duplicacy.com/t/restore-to-a-different-folder-or-computer/1103
+echo -e "\e[32m---> Downloading duplicacy binary $DUPLICACY \e[39m";
+lxc exec $HOST -- sh -c " \
+  wget https://github.com/gilbertchen/duplicacy/releases/download/v2.2.3/$DUPLICACY; \
+  chmod u+x $DUPLICACY; \
+  mv ./$DUPLICACY /bin; \
+";
