8f54c2a9dbdade3705873b9325cbb16d94bfa74f
Frostlock <pieter.boogaerts@gmail.com>
2019-08-22 10:33:47 +0200

    added some software

--- scripts/workstation_postinstall.sh
+++ scripts/workstation_postinstall.sh
@@ -40,3 +40,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y -q
+# Baobab disk analyzer
+sudo apt-get install baobab
+# Text editor
+sudo apt-get install nano
